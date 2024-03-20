66b9aa058a7786f3086735bc52dda3329a076459
willianifms <williancardoso00004@gmail.com>
2024-03-19 21:32:20 -0400

    tomara que isso funcione futuramente

--- frontEnd/src/app/page.js
+++ frontEnd/src/app/page.js
@@ -15,52 +15,52 @@ export default async function Home() {
   const formatarData = (date) => {
     return format(parseISO(date), "dd/MM/yyyy HH:mm", { locale: ptBR })
   }
-  
+
   const remover = (id) => {
     const codigo = { id: parseInt(id) }
     const idJson = JSON.stringify(codigo);
 
     try {
-        fetch("http://localhost:3003/produtos", {
-          method: "DELETE",
-          headers: { 'content-type': 'application/json' },
-          body: idJson
-        })
-        router.refresh();
+      fetch("http://localhost:3003/produtos", {
+        method: "DELETE",
+        headers: { 'content-type': 'application/json' },
+        body: idJson
+      })
+      router.refresh();
     } catch (error) {
-        console.log("Ocorreu um erro" + error)
+      console.log("Ocorreu um erro" + error)
     }
-}
+  }
   return (
-    <main> 
+    <main>
       <div className='cadastro'>
         <p>MultCoisas</p>
-      <Link href="/cadastro" className='voltar'> CADASTRAR </Link>
+        <Link href="/cadastro" className='voltar'> CADASTRAR </Link>
       </div>
 
 
       <div className='produto'>
-  {produtos.map(produtos => (
-    <div className='produto-card' key={produtos.id}>
-      <div className='produto-imagem'>
-        <img className='imagem-produto' src={produtos.imagem} alt={produtos.titulo} />
+        {produtos.map(produtos => (
+          <div className='produto-card' key={produtos.id}>
+            <div className='produto-imagem'>
+              <img className='imagem-produto' src={produtos.imagem} alt={produtos.titulo} />
+            </div>
+            <div className='produto-conteudo'>
+              <h3 className='produto-titulo'>{produtos.titulo.slice(0, 50)}</h3>
+              <p className='produto-data'>{formatarData(produtos.data_cadastro).slice(0, 10)}</p>
+              <p className='produto-preco'>R$ {produtos.preco}</p>
+              {/*<p className='produto-descricao'>{produtos.descricao.slice(0, 100)}</p> */}
+              <div className='socorro'>
+                <Link href={`/produto/${produtos.id}`} className='ver-mais-linkV'>ver mais</Link>
+                <Link href={`/alterar/${produtos.id}`} className='ver-mais-linkA'>alterar</Link>
+                <button onClick={() => remover(produtos.id)} className='excluir-button'>excluir</button>
+              </div>
+            </div>
+          </div>
+        ))}
       </div>
-      <div className='produto-conteudo'>
-        <h3 className='produto-titulo'>{produtos.titulo}</h3>
-        <p className='produto-data'>{formatarData(produtos.data_cadastro).slice(0, 10)}</p>
-        <p className='produto-preco'>R$ {produtos.preco}</p>
-        <p className='produto-descricao'>{produtos.descricao}</p>
-        <div className='socorro'>
-          <Link href={`/produto/${produtos.id}`} className='ver-mais-link'>ver mais</Link>
-          <Link href={`/alterar/${produtos.id}`}  className='ver-mais-link'>alterar</Link>
-          <button onClick={() => remover(produtos.id)} className='excluir-button'>excluir</button> 
-        </div>
-      </div>
-    </div>
-  ))}
-</div>
 
-      
+
     </main>
   )
 }
